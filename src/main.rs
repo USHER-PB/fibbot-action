8b5454de5b82be7dcadf24975cd217ef3db73145
USHER-PB <ushertchankoumi9@gmail.com>
2025-03-01 16:20:34 +0100

    fetch

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ async fn main() -> Result<()> {
     println!("{}", max_threshold);
 
 
-    let enable_fib:bool = enable_fib.trim().parse().expect("fail");
+    let enable_fib:bool = enable_fib.trim().parse().unwrap_or(true);
     let max_threshold:u128 = max_threshold.trim().parse().expect("fail");
     // Fetching environment variables\
     // let input_enable_fib = env::var("INPUT_ENABLE_FIB").unwrap_or("true".to_string());
@@ -45,7 +45,7 @@ async fn main() -> Result<()> {
     println!("PR Content: {}", pr_content);
 
     // Process PR content if Fibonacci calculation is enabled
-    if "enable_fib" == "true" {
+    if enable_fib == true {
         let integers = extract_integer_strings(&pr_content);
 
         for number in integers {
