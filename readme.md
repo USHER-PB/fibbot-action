fd7be21896159d943d1ea4d13fea868f0b8e450a
USHER-PB <ushertchankoumi9@gmail.com>
2025-03-03 07:36:15 +0100

    feat added

--- readme.md
+++ readme.md
@@ -2,4 +2,42 @@
 
 ## INTRODUCTION
 
-This project is based on calculating the fibonnaci of a given set of values and extracting their result so as to be post as comment when a pull request is been made 
+This project is based on calculating the fibonnaci of a given set of values and extracting their result so as to be post as comment when a pull request is been made.
+
+
+## USAGE
+Firstly the fibonnaci of a given set of values are calculated and their result are place as comment when a pr is been made.
+
+- So for this i have use a workflow that will be trigger each time my pr is been made 
+- A dockerfile so as to build and run my image 
+
+- If you want to take a look on the repository  start by doing  
+
+      git clone https://github.com/USHER-PB/fibbot-aciton.git
+   
+   to have  the actual repository locally on your machine 
+
+-  run the program using 
+
+              cargo run     
+
+- then build your dockerfile and run your docker image by running 
+
+      docker build -t fibbot .
+  to build your docker image and finally 
+
+      docker run -it --rm --name<container name > fibbot
+  to run you docker image 
+
+  ### Contribution
+
+  if you are interested on bringing some small changes to the repository i invite you to look at this 
+
+       git pull https://github.com/USHER-PB/fibbot-aciton.git
+
+
+
+and finaly make a pull request so as for your contibution to be admit and merge to the main branch.
+    
+
+
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ async fn main() -> Result<()> {
     let max_threshold = env::var("INPUT_MAX_THRESHOLD").unwrap_or("100".to_string());
 
     println!("{}", max_threshold);
+    let string = "good bad 34 12";
+    let output = extract_integer_strings(string);
 
 
     let enable_fib:bool = enable_fib.trim().parse().unwrap_or(true);
@@ -22,37 +24,37 @@ async fn main() -> Result<()> {
     //     .parse()
     //     .context("Failed to parse INPUT_MAX_THRESHOLD")?;
 
-    // // Extract PR number from GITHUB_REF
-    let pr_number: u32 = env::var("GITHUB_REF")
-    .ok()
-    .and_then(|ref_value| {
-        if ref_value.starts_with("refs/pull/") {
-            ref_value
-                .split('/')
-                .nth(2)
-                .and_then(|num| num.parse().ok())
-        } else {
-            None
-        }
-    })
-    .context("Failed to parse PR number from GITHUB_REF. Please ensure the workflow is triggered by a pull request.")?;
+    // Extract PR number from GITHUB_REF
+    // let pr_number: u32 = env::var("GITHUB_REF")
+    // .ok()
+    // .and_then(|ref_value| {
+    //     if ref_value.starts_with("refs/pull/") {
+    //         ref_value
+    //             .split('/')
+    //             .nth(2)
+    //             .and_then(|num| num.parse().ok())
+    //     } else {
+    //         None
+    //     }
+    // })
+    // .context("Failed to parse PR number from GITHUB_REF. Please ensure the workflow is triggered by a pull request.")?;
 
     // Fetch PR content
-    let pr_content = fetch_pr_content("USHER-PB", "Fibbot-action", pr_number)
-        .await
-        .context("Failed to fetch PR content")?;
+    // let pr_content = fetch_pr_content("USHER-PB", "Fibbot-action")
+    //     .await
+    //     .context("Failed to fetch PR content")?;
 
-    println!("PR Content: {}", pr_content);
+    // println!("PR Content: {}", pr_content);
 
     // Process PR content if Fibonacci calculation is enabled
     if enable_fib == true {
-        let integers = extract_integer_strings(&pr_content);
+        let integers = output;
 
         for number in integers {
             if number < max_threshold {
                 // Handle the Result returned by fibo_calculator
                 if let Err(e) = fibo_calculator(number).await {
-                    eprintln!("Error calculating Fibonacci for {}: {}", number, e);
+                    // eprintln!("Error calculating Fibonacci for {}: {}", number, e);
                 }
             }
         }
@@ -61,26 +63,26 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-async fn fetch_pr_content(owner: &str, repo: &str, pr_number: u32) -> Result<String> {
-    let token = env::var("GITHUB_TOKEN").context("GITHUB_TOKEN not set")?;
-    let client = Client::new();
-    let url = format!(
-        "https://api.github.com/repos/{}/{}/pulls/{}",
-        owner, repo, pr_number
-    );
+// async fn fetch_pr_content(owner: &str, repo: &str,) -> Result<String> {
+//     let token = env::var("GITHUB_TOKEN").context("GITHUB_TOKEN not set")?;
+//     let client = Client::new();
+//     let url = format!(
+//         "https://api.github.com/repos/{}/{}/pulls/",
+//         owner, repo
+//     );
 
-    let response = client
-        .get(&url)
-        .header("Authorization", format!("Bearer {}", token))
-        .header("Content-Type", "application/vnd.github+json")
-        .header("User-Agent", "USHER-PB")
-        .send()
-        .await
-        .context("Failed to send request to GitHub API")?;
+//     let response = client
+//         .get(&url)
+//         .header("Authorization", format!("Bearer {}", token))
+//         .header("Content-Type", "application/vnd.github+json")
+//         .header("User-Agent", "USHER-PB")
+//         .send()
+//         .await
+//         .context("Failed to send request to GitHub API")?;
 
-    let response_text = response.text().await.context("Failed to read response body")?;
-    Ok(response_text)
-}
+//     let response_text = response.text().await.context("Failed to read response body")?;
+//     Ok(response_text)
+// }
 
 async fn post_comment(body: String) -> Result<()> {
     let client = Client::new();
@@ -109,6 +111,7 @@ async fn fibo_calculator(number: u128) -> Result<()> {
 
     if number == 0 {
         post_comment(format!("The Fibonacci value of {} is {}", number, a)).await?;
+        println!("the fibonnaci of {} is {}", number,a);
         return Ok(());
     }
 
@@ -117,8 +120,8 @@ async fn fibo_calculator(number: u128) -> Result<()> {
         a = b;
         b = pre_fib;
         if i == number {
-            post_comment(format!("The Fibonacci value of {} is {}", number, b)).await?;
-            
+            println!("the fibonnaci of {} is {}", number,b);
+            post_comment(format!("The Fibonacci value of {} is {}", number, b)).await?;          
         }
     }
 
